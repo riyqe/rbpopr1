024b81b1b4fed6e270b9ec3004b11554ad12bfbd
riyqe <64642790+riyqe@users.noreply.github.com>
2025-12-08 22:00:00 +0300

    фиксы 6лаб

--- src/main/java/ru/mtuci/coursemanagement/config/WebConfig.java
+++ src/main/java/ru/mtuci/coursemanagement/config/WebConfig.java
@@ -1,17 +1,29 @@
 package ru.mtuci.coursemanagement.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("*")
-                .allowedHeaders("*");
+public class WebConfig {
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                // отключаем CSRF для API (для ZAP, чтобы мог тестировать POST),
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers
+                        // защита от Clickjacking (X-Frame-Options)
+                        .frameOptions(frame -> frame.sameOrigin())
+                        // защита от сниффинга (X-Content-Type-Options)
+                        .contentTypeOptions(withDefaults())
+                );
+
+        return http.build();
     }
 }
